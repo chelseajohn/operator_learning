286b89bae88ef6559f5fe40a330ed8e3946b9931
john2 <john2@jrlogin08.jureca>
2025-10-02 16:50:01 +0200

    remove prob specific import from init files

--- operator_learning/data/__init__.py
+++ operator_learning/data/__init__.py
@@ -1,18 +1,7 @@
-from .problems.rbc2D.dedalus_simu import runSimu
-from .problems.rbc2D.dedalus_prop import OutputFiles
-from .hdf5_dataset import RBCDataset, DomainDataset
-from .pysdc_dataset import PySDCReader
-from .pic_dataset import PICDataset
 from .transforms.vandermonde import VandermondeTransform
 from .utils import getDataLoaders
 
 __all__ = [
-    "runSimu",
-    "OutputFiles",
-    "RBCDataset",
-    "DomainDataset",
-    "PySDCReader",
-    "PICDataset",
     "VandermondeTransform",
     "getDataLoaders",
 ]
